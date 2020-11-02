588b4daca151a646ddd3c1ac355ddaa4acf5cc1f
simpybest <65767608+simpybest@users.noreply.github.com>
2020-11-03 00:38:17 +0300

    autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const minCss = require('gulp-cssmin');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
 // require "name" located in pachage.json
 
 // server
@@ -24,6 +25,9 @@ function bs() {
 function serveSass () {
     return src('src/sass/*.sass')
         .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest('src/css'))
         .pipe(browserSync.stream());
 };
