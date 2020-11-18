d9bd824572a5d57d7f4f3c2ffa70eecf71dbe253
simpybest <65767608+simpybest@users.noreply.github.com>
2020-11-18 17:40:42 +0300

    updatet full homework

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,106 @@
-
-document.addEventListener("DOMContentLoaded", function (event) {
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector('.modal__close');
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible');
-    }
-
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal);
-    });
-    closeBtn.addEventListener('click', switchModal);
-});
-
-/*
-$(document).ready function () {
+$(document).ready(function() {
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
         closeBtn = $('.modal__close');
 
-    modalBtn.on('click', function () {
-        modal.toggleClass('modal--visible');
+    modalBtn.on('click', function() {
+        modal.toggleClass('modal--visible')
+    });
+    closeBtn.on('click', function() {
+        modal.toggleClass('modal--visible')
+    });
 
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+
+    next.css('left', prev.width() + 10 + bullets.width() + 10);
+    bullets.css('left', prev.width() + 10);
+
+    new WOW().init();
+
+    $('.modal__form').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userPhone: "required",
+            userEmail: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Имя не короче двух букв"
+            },
+            userPhone: "Телефон обязателен",
+            userEmail: {
+                required: "Обязательно укажите e-mail",
+                email: "Введите в формате: name@domain.com"
+            }
+        }
     });
-    closeBtn.on('click', function (){
-        modal.toggleClass('modal--visible');
+    $('.footer__form').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userPhone: "required",
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Имя не короче двух букв"
+            },
+            userPhone: "Телефон обязателен",
+        }
     });
+    $('.control__form').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userPhone: "required",
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Имя не короче двух букв"
+            },
+            userPhone: "Телефон обязателен",
+        }
+    });
+
+    $('[type=tel]').mask('+7(000)-000-00-00', { placeholder: "+7(___)-___-__-__" });
+
 });
+
+/*
+    document.addEventListener('keyup', (event) => {
+        var keyName = event.key;
+        if (keyName === 'Escape') {
+            modal.toggleClass('modal--visible')
+        }
+
+    }, false);
 */
