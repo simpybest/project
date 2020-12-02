364f20b1df43ab6fbbc713743b9ea1da64dce787
simpybest <65767608+simpybest@users.noreply.github.com>
2020-12-02 17:31:50 +0300

    video

--- src/js/main.js
+++ src/js/main.js
@@ -92,7 +92,21 @@ $(document).ready(function() {
     });
 
     $('[type=tel]').mask('+7(000)-000-00-00', { placeholder: "+7(___)-___-__-__" });
-
+    var player;
+    $('.video__play').on('click', function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '465',
+          width: '100%',
+          videoId: 'RHzzLqJWqHs',
+          events: {
+            'onReady': videoPlay,
+          }
+        });
+      }
+)
+function videoPlay() {
+    player.videoPlay();
+}
 });
 
 /*
